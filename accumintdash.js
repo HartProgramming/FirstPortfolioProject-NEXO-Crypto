de77bc3eb7bc04192e5e24383e07e53039051cb0
Web3Learner <programmedhart@protonmail.com>
2022-02-27 12:30:41 -0500

    commit

---
+++ accumintdash.js
@@ -0,0 +1,8 @@
+const btcImg = document.querySelector("#bitcoin");
+const row = document.querySelector("#token-row");
+const rowInsert = row.insertRow(1);
+const cell1 = rowInsert.insertCell(0);
+
+btcImg.addEventListener("click",function(){
+    console.log("hello")
+})

e7c360849ca00e0440c05b9956f3809320533172
Web3Learner <programmedhart@protonmail.com>
2022-02-28 15:44:58 -0500

    commit

--- accumintdash.js
+++ accumintdash.js
@@ -69,26 +69,26 @@ class Crypto {
     }
 }
 
-const bitcoin = new Crypto("bitcoin-btc-logo.png", "Bitcoin", 50, 40)
+const bitcoin = new Crypto("bitcoin-btc-logo.png", "Bitcoin", 30, 40)
 const ethereum = new Crypto("ethereum-eth-logo.png", "Ethereum", 50, 40)
 
-function insRow(x, name){
+function insRow(name){
     let tokenImage = document.createElement("img")
     tokenImage.src = name.pic
     tokenImage.height = 25;
-    let rowInsert = row.insertRow(x);
+    let rowInsert = row.insertRow();
     const cell0 = rowInsert.insertCell(-1);
     const cell1 = rowInsert.insertCell(1);
     const cell2 = rowInsert.insertCell(2);
     const cell3 = rowInsert.insertCell(3);
     const cell4 = rowInsert.insertCell(4);
-    cell4.setAttribute("contenteditable", true)
     cell4.setAttribute("style", "background-color: red;")
+    cell4.setAttribute("contenteditable", false)
     cell4.textContent = "500"
-    cell4.value = cell4.textContent
+    cell4.height = 15;
     const cell5 = rowInsert.insertCell(5);
-    cell5.textContent = parseFloat(cell4.value * cell2.textContent)
-    console.log(cell5.textContent)
+    cell5.innerHTML = parseFloat(cell4.textContent * cell2.textContent)
+    console.log(cell5.innerHTML)
     const cell6 = rowInsert.insertCell(6);
     cell6.value = parseFloat(cell4.textContent * cell3.textContent)
     cell6.innerHTML = cell6.value;
@@ -97,31 +97,69 @@ function insRow(x, name){
     cell7.textContent = parseFloat(cell5.textContent / cell6.textContent)
     console.log(cell7.textContent)
     const cell8 = rowInsert.insertCell(8);
-    cell8.setAttribute("contenteditable", true)
+    cell8.classList.add("dropdown")
+    cell8.classList.add("btn")
+    cell8.classList.add("btn-secondary")
+    cell8.classList.add("dropdown-toggle")
+    cell8.setAttribute("type", "button")
+    cell8.textContent = "Credit";
     cell8.setAttribute("style", "background-color: red;")
     const cell9 = rowInsert.insertCell(9);
     cell9.setAttribute("type", "button")
     cell9.setAttribute("style", "background-color: orange;")
-    cell9.textContent = "Edit"
+    cell9.textContent = "Edit";
+    cell9.addEventListener("click", function(){
+        cell4.setAttribute("contenteditable", true);
+        cell4.addEventListener("keydown",function(event){
+            if(event.keyCode === 13){
+                event.preventDefault()
+                cell4.setAttribute("contenteditable", false)
+                cell5.textContent = parseFloat(cell4.textContent * cell2.textContent)
+                console.log(cell5.textContent)
+                cell6.textContent =  parseFloat(cell4.textContent * cell3.textContent);
+                cell7.textContent = Math.floor((cell5.textContent / cell6.textContent) * 100) - 100 + "%" ;
+                function cvSum() {
+                    const port = document.querySelector("#port-val");
+                    let portArr = []
+                    portArr.push(cell5.textContent)
+                    console.log(portArr)
+                }
+                cvSum()
+            }
+        })
+        cell8.addEventListener("keydown", function(event){
+            if(event.keyCode === 13){
+                cell8.setAttribute("contenteditable", false)
+                
+            }
+        })
+    })
     const cell10 = rowInsert.insertCell(10);
     cell10.setAttribute("type", "button")
     cell10.setAttribute("style", "background-color: red;")
     cell10.setAttribute("textContent", "delete")
     cell10.textContent = "Delete"
+    cell10.addEventListener("click", function(){
+        rowInsert.remove()
+    })
     cell0.appendChild(tokenImage);
     cell1.append(name.name);
     cell2.textContent = name.price;
     cell3.textContent = name.snap;
+
+    
 }
 
+/* Sums up Current Value Columns */
+
+
+
 eth.addEventListener("click", function () {
-    insRow(count, ethereum)
-    count += 1;
+    insRow(ethereum)
 })
 
 btcImg.addEventListener("click",function(){
-    insRow(count, bitcoin) 
-    count += 1;   
+    insRow(bitcoin) 
 })
 
 cosmos.addEventListener("click",function(){
